db9c497028e34f2dc8a7e41381fdc77580f7d69c
bstriker <strikeforcezero@gmail.com>
2025-06-17 12:36:34 -0400

    feat(player): add grunt noise to player on wall touch

--- components/player/player.gd
+++ components/player/player.gd
@@ -6,6 +6,8 @@ const SPEED = 5.0
 
 var target_dir = Vector3.FORWARD
 
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
+
 func _process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -21,3 +23,27 @@ func _process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	handle_collide_effects()
+
+var grunting = false
+func handle_collide_effects():
+	for idx in get_slide_collision_count():
+		var col := get_slide_collision(idx)
+		if col.get_collider() is Monster:
+			continue
+		if abs(get_wall_normal().x) == 1 or abs(get_wall_normal().z) == 1:
+			grunt()
+
+func grunt():
+	if grunting:
+		return
+	grunting = true
+	audio_player.autoplay = true
+	audio_player.stream = AudioStreamOggVorbis.load_from_file("res://assets/sounds/grunt-106134.ogg")
+	audio_player.finished.connect(grunt_done)
+	audio_player.play(0.3)
+	print("grunt")
+
+func grunt_done():
+	grunting = false
+	
