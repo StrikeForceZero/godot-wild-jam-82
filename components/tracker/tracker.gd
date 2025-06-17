ddd99ba1e318835dd6d0226bbadb687ad4fb0263
bstriker <strikeforcezero@gmail.com>
2025-06-17 16:14:06 -0400

    refactor(tracker): cleanup audio positioning and remove raycast

--- components/tracker/tracker.gd
+++ components/tracker/tracker.gd
@@ -6,10 +6,10 @@ signal on_hit(position)
 
 @export var speed: float = 300.0
 @export var max_radius: float = 300.0
+@export var ears: AudioListener3D
 var radius: float = 0.25
 
 @onready var scan_area: Area3D = %ScanArea
-@onready var ray_cast: RayCast3D = %RayCast
 @onready var tracker_screen: TrackerScreen = %TrackerScreen
 @onready var bip_audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
@@ -19,7 +19,7 @@ const MAX_PITCH = 1.5
 
 const MAX_DISTANCE = 50.0
 const MIN_VOLUME_DB = -25.0  # -40.0 Silence
-const MAX_VOLUME_DB = -15.0    # Full volume
+const MAX_VOLUME_DB = -0.0    # Full volume
 
 func _physics_process(delta: float) -> void:
 	radius += speed * delta
@@ -32,19 +32,12 @@ func _physics_process(delta: float) -> void:
 	
 
 func _on_scan_area_body_entered(body: Node3D) -> void:
-	var hit_location = body.global_position - ray_cast.global_position
-	ray_cast.target_position = ray_cast.to_local(body.global_position)
-	var offset = (ray_cast.target_position - ray_cast.position)
-	ray_cast.target_position += offset
-	var collider = ray_cast.get_collider()
-	print(body, " hit_location: ", hit_location, " collider: ", collider)
-	if collider == null:
-		# return
-		pass
-	on_hit.emit(hit_location)
+	on_hit.emit(body.global_position)
 	var audio := AudioStreamPlayer3D.new()
-	var dir := body.global_position - global_position
-	var distance := dir.length()
+	var offset := body.global_position - global_position
+	var ear_dir := (body.global_position - ears.global_position).normalized()
+	var dir := offset.normalized()
+	var distance := offset.length()
 	
 	# Map distance to volume
 	var t = clamp(1.0 - (distance / MAX_DISTANCE), 0.0, 1.0)
@@ -52,20 +45,16 @@ func _on_scan_area_body_entered(body: Node3D) -> void:
 	audio.volume_db = volume_db
 	
 	# Pan the audio
-	#var relative := dir.normalized()
 	#var listener_basis := global_transform.basis
-	#var local: Vector3 = listener_basis.inverse() * relative
+	#var local: Vector3 = listener_basis.inverse() * dir
 	#audio.panning_strength = clamp(local.x, -100.0, 100.0)  # Pan between -1 (left) and 1 (right)
 	
 	var pitch_scale = REFERENCE_DISTANCE / distance
 	pitch_scale = clamp(pitch_scale, MIN_PITCH, MAX_PITCH)
 	audio.pitch_scale = pitch_scale
 	
-	#audio.global_position = global_position
-	audio.global_position = body.global_position
-	audio.unit_size = 50.0
-	audio.max_distance = 500.0
-	audio.attenuation_model = AudioStreamPlayer3D.AttenuationModel.ATTENUATION_LOGARITHMIC
-	audio.attenuation_filter_db = 0.0
-	# audio.doppler_tracking = AudioStreamPlayer3D.DopplerTracking.DOPPLER_TRACKING_PHYSICS_STEP
+	# place sound directionally away from ears
+	audio.global_position = ears.global_position + ear_dir
+	audio.unit_size = 1.0
+	audio.max_distance = 2.0
 	GlobalUtil.play_audio("res://assets/sounds/beep-313342.ogg", audio)
